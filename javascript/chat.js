5fbe56fc14e7a279e0e78df60f500fc8efdeafb9
Juliot <njuliotjustin@gmail.com>
2024-06-07 18:22:54 +0200

    mise en ligne de mon premier site web

---
+++ javascript/chat.js
@@ -0,0 +1,58 @@
+const form = document.querySelector(".typing-area"),
+inputField = form.querySelector(".input-field"),
+sendBtn = form.querySelector("button"),
+chatBox = document.querySelector(".chat-box");
+
+form.onsubmit = (e)=>{
+    e.preventDefault(); //preventing form from submitting
+}
+
+sendBtn.onclick = ()=>{
+       // let's start ajax
+       let xhr = new XMLHttpRequest();  //creating XML object
+       xhr.open("POST", "php/insert-chat.php", true);
+       xhr.onload = ()=>{
+           if(xhr.readyState === XMLHttpRequest.DONE){
+               if(xhr.status === 200){
+                inputField.value = ""; //once message inserted into database then lesave blank the input field
+                scrollBottom();
+            }
+           }
+       }
+       //we have to send the form data through ajax to php
+       let formData= new FormData(form); //creating new FormData
+       xhr.send(formData);
+}
+chatBox.onmouseenter = ()=>{
+    chatBox.classList.add("active");
+}
+chatBox.onmouseleave = ()=>{
+    chatBox.classList.remove("active");
+}
+
+
+setInterval(()=>{
+    // let's start ajax
+    let xhr = new XMLHttpRequest();  //creating XML object
+    xhr.open("POST", "php/get-chat.php", true);
+    xhr.onload = ()=>{
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status === 200){
+                 let data = xhr.response;
+                 chatBox.innerHTML = data;
+                 if(!chatBox.classList.contains("active")){
+                    scrollBottom();
+                 }
+            }
+        }
+    }
+    //we have to send the form data through ajax to php
+    let formData= new FormData(form); //creating new FormData
+    xhr.send(formData);
+
+}, 500); //this fucntion will run frequently after 500ms
+
+function scrollBottom() {
+    chatBox.scrollTop = chatBox.scrollHeight;
+    
+}
